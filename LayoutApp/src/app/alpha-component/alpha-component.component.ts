f0edad083fde5bb9af3e88c92230533d9522f1ef
Lindsay Gale <lindsay.gale@mlboi-lga03.local>
2018-04-17 22:40:40 -0600

    Adding Angular assignment - LayoutApp.

---
+++ LayoutApp/src/app/alpha-component/alpha-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alpha-component',
+  templateUrl: './alpha-component.component.html',
+  styleUrls: ['./alpha-component.component.css']
+})
+export class AlphaComponentComponent implements OnInit {
+
+  alphaTitle: string = "AlphaComponent";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ LayoutApp/src/app/alpha-component/beta-component/gamma-component/gamma-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gamma-component',
+  templateUrl: './gamma-component.component.html',
+  styleUrls: ['./gamma-component.component.css']
+})
+export class GammaComponentComponent implements OnInit {
+
+  gammaTitle: string = "GammaComponent";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
